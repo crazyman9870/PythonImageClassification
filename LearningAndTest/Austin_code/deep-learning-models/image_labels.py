5893d32d841e438f059d419e45bbfae8fc3510e1
Austin <atsoderquist@gmail.com>
2017-03-29 23:47:54 -0600

    added files

---
+++ LearningAndTest/Austin_code/deep-learning-models/image_labels.py
@@ -0,0 +1,59 @@
+from joblib import Parallel, delayed
+import multiprocessing
+
+from keras.preprocessing import image as image_utils
+from imagenet_utils import decode_predictions
+from imagenet_utils import preprocess_input
+from vgg16 import VGG16
+
+import numpy as np
+import argparse
+import os
+import csv
+
+ap = argparse.ArgumentParser()
+ap.add_argument('-i', '--images', required=True, help='path to the folder with input images')
+ap.add_argument('-r', '--results', required=True, help='path to the csv with results')
+ap.add_argument('-l', '--labels', required=True, help='csv file name that will store image file names')
+args = vars(ap.parse_args())
+
+def getLabels(image_location):
+    loc = open(image_location, 'r')
+
+    image = image_utils.load_img(loc.name, target_size=(224, 224))
+    image = image_utils.img_to_array(image)
+    image = np.expand_dims(image, axis=0)
+    image = preprocess_input(image)
+
+    model = VGG16(weights='imagenet')
+
+    preds = model.predict(image)
+    predictions = decode_predictions(preds, 1000)[0]
+    predictions.sort(key=lambda tup: tup[1])
+
+    percentages = [x[2] for x in predictions]
+
+    loc.close()
+
+    return percentages
+
+def writeCSV(file_name, data):
+    with open(file_name, 'w') as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+    f.close()
+
+images = []
+image_locations = []
+for root, dirs, filenames in os.walk(args['images']):
+    for f in filenames:
+        file_location = os.path.join(root, f)
+        images.append((f, file_location))
+        image_locations.append(file_location)
+
+num_cores = multiprocessing.cpu_count()
+
+predictions = Parallel(n_jobs=num_cores)(delayed(getLabels)(filepath) for filepath in image_locations)
+
+writeCSV(args['results'], predictions)
+writeCSV(args['labels'], images)
